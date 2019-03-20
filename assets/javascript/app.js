84d104008459bed202ebbc3383ccde81ec0e51a2
J91Branson <jbcod4@gmail.com>
2019-03-19 22:27:10 -0500

    updated navigation for mobile responsiveness

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,36 @@
+
 var config = {
-    apiKey: "AIzaSyButx5BRRMUZ_XsaEyqIvS2PgqDCdRxBGA",
-    authDomain: "myportfolio-e0745.firebaseapp.com",
-    databaseURL: "https://myportfolio-e0745.firebaseio.com",
-    projectId: "myportfolio-e0745",
-    storageBucket: "myportfolio-e0745.appspot.com",
-    messagingSenderId: "618355092736"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyButx5BRRMUZ_XsaEyqIvS2PgqDCdRxBGA",
+  authDomain: "myportfolio-e0745.firebaseapp.com",
+  databaseURL: "https://myportfolio-e0745.firebaseio.com",
+  projectId: "myportfolio-e0745",
+  storageBucket: "myportfolio-e0745.appspot.com",
+  messagingSenderId: "618355092736"
+};
+firebase.initializeApp(config);
 
-  
-  
-  var database = firebase.database();
-  var info = database.ref('/contactMessage');
 
-  $("#submit-btn").on("click", function (event) {
-    event.preventDefault();
 
-    var fName = $("#fname").val().trim();
-    var lName = $("#lname").val().trim();
-    var email = $("#email").val().trim();
-    var text = $("#text").val().trim();
+var database = firebase.database();
+var info = database.ref('/contactMessage');
 
-    var newInfo ={
-        fName: fName,
-        lastName: lName,
-        email: email,
-        text: text,
-    }
-    database.ref().push(newInfo);
-    $("#fname").val("");
-    $("#lname").val("");
-    $("#email").val("");
-    $("#text").val("");
+$("#submit-btn").on("click", function (event) {
+  event.preventDefault();
+
+  var fName = $("#fname").val().trim();
+  var lName = $("#lname").val().trim();
+  var email = $("#email").val().trim();
+  var text = $("#text").val().trim();
+
+  var newInfo ={
+      fName: fName,
+      lastName: lName,
+      email: email,
+      text: text,
+  }
+  database.ref().push(newInfo);
+  $("#fname").val("");
+  $("#lname").val("");
+  $("#email").val("");
+  $("#text").val("");
 });
