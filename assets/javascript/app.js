b28cae81e61873f3725bac43b3d326c3d13e8a1d
J91Branson <jbcod4@gmail.com>
2019-04-01 13:07:54 -0500

    Hamburger test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,9 @@
-
-var config = {
-  apiKey: "AIzaSyButx5BRRMUZ_XsaEyqIvS2PgqDCdRxBGA",
-  authDomain: "myportfolio-e0745.firebaseapp.com",
-  databaseURL: "https://myportfolio-e0745.firebaseio.com",
-  projectId: "myportfolio-e0745",
-  storageBucket: "myportfolio-e0745.appspot.com",
-  messagingSenderId: "618355092736"
-};
-firebase.initializeApp(config);
-
-
-
-var database = firebase.database();
-var info = database.ref('/contactMessage');
-
-$("#submit-btn").on("click", function (event) {
-  event.preventDefault();
-
-  var fName = $("#fname").val().trim();
-  var lName = $("#lname").val().trim();
-  var email = $("#email").val().trim();
-  var text = $("#text").val().trim();
-
-  var newInfo ={
-      fName: fName,
-      lastName: lName,
-      email: email,
-      text: text,
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
-  database.ref().push(newInfo);
-  $("#fname").val("");
-  $("#lname").val("");
-  $("#email").val("");
-  $("#text").val("");
-});
+}
